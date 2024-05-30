2840926c1b1773e8f172d7a356bf1db61e371e03
Rasmus Larsson <rasmus.larsson@accenture.com>
2024-05-30 09:40:14 +0300

    Cleanup
    Fixed grammar and typos.
    Moved printouts to clarify intentions.

--- install-influxdb.sh
+++ install-influxdb.sh
@@ -10,26 +10,36 @@ else
   export INFLUXDB_DOWNLOAD_PATH="https://dl.influxdata.com/influxdb/releases"
 fi
 
-echo "Thre Server downloading "
+echo "The Server is downloading "
 wget -q $INFLUXDB_DOWNLOAD_PATH/influxdb2_$INFLUXDB_VERSION-linux-amd64.tar.gz
 tar xvfz influxdb2-$INFLUXDB_VERSION-linux-amd64.tar.gz
-cp influxdb2_linux_amd64/influxd /usr/local/bin/
-rm -r influxdb2_linux_amd64/
-echo "Thre Client downloading "
+if [  -x "/tmp/influxdb2_linux_amd64/influxd" ]; then
+  cp influxdb2_linux_amd64/influxd /usr/local/bin/
+  rm -r influxdb2_linux_amd64/
+else
+  echo "The server is not found.."
+  ls /tmp/influxdb2-client/
+  rm -r influxdb2_linux_amd64/
+  exit 123
+fi
+
+echo "The Client is downloading "
 wget -q $INFLUXDB_DOWNLOAD_PATH/influxdb2-client-$INFLUXDB_VERSION-linux-amd64.tar.gz
 mkdir  /tmp/influxdb2-client/ 
 tar xvfz influxdb2-client-$INFLUXDB_VERSION-linux-amd64.tar.gz -C /tmp/influxdb2-client/
-echo "The client in subdir?"
-if [  -x "/tmp/influxdb2-client/influxdb2-client-$INFLUXDB_VERSION-linux-amd64/influx" ];then  
-echo "The client in subdir."
-cp /tmp/influxdb2-client/influxdb2-client-$INFLUXDB_VERSION-linux-amd64/influx /usr/local/bin/
-rm -r /tmp/influxdb2-client/
-fi
-echo "The client not in subdir?"
-if [  -x "/tmp/influxdb2-client/influx" ];then  
-echo "The client not in subdir."
-cp /tmp/influxdb2-client/influx /usr/local/bin/
-rm -r /tmp/influxdb2-client/
+
+if [  -x "/tmp/influxdb2-client/influxdb2-client-$INFLUXDB_VERSION-linux-amd64/influx" ]; then
+  echo "The client is in subdir."
+  cp /tmp/influxdb2-client/influxdb2-client-$INFLUXDB_VERSION-linux-amd64/influx /usr/local/bin/
+elif [  -x "/tmp/influxdb2-client/influx" ]; then
+  echo "The client is in basedir."
+  cp /tmp/influxdb2-client/influx /usr/local/bin/
+  rm -r /tmp/influxdb2-client/
+else
+  echo "The client is not found.."
+  ls /tmp/influxdb2-client/
+  rm -r /tmp/influxdb2-client/
+  exit 123
 fi
 
 cd -
